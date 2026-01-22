9f30524fff99a19ffff70c0be5e836d66fe8b93c
jlamilladouble <jose.lamilla@doublevpartners.com>
2026-01-21 23:49:53 -0500

    feat: add StoycoTooltip and TooltipButton components for enhanced tooltip functionality

---
+++ lib/design/atomic/atoms/tooltip/tooltip_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:stoyco_partners_shared/design/atomic/atoms/tooltip/tooltip.dart';
+import 'package:stoyco_partners_shared/design/responsive/screen_size/stoyco_screen_size.dart';
+import 'package:stoyco_partners_shared/design/utils/tokens/gen/assets.gen.dart';
+
+class TooltipButton extends StatelessWidget {
+  const TooltipButton({
+    super.key,
+    required this.tooltipMessage,
+    this.padding,
+    this.width = 18.0,
+    this.height = 18.0,
+    this.maxWidth = 116.0,
+    this.direction = TooltipDirection.top,
+  });
+
+  final String tooltipMessage;
+  final EdgeInsetsGeometry? padding;
+  final double width;
+  final double height;
+  final double maxWidth;
+  final TooltipDirection direction;
+
+  @override
+  Widget build(BuildContext context) {
+    return StoycoTooltip(
+      message: tooltipMessage,
+      maxWidth: StoycoScreenSize.width(context, maxWidth),
+      direction: direction,
+      child: Container(
+        padding: padding,
+        child: StoycoAssetsToken.lib.assets.icons.common.interrogation.svg(
+          package: 'stoyco_partners_shared',
+          width: StoycoScreenSize.width(context, width),
+          height: StoycoScreenSize.height(context, height),
+        ),
+      ),
+    );
+  }
+}
