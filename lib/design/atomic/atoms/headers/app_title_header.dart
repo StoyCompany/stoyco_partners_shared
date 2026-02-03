39461b0e31c78f3b48a03297c7ecc919d10fea30
Jose Lamilla <jose.lamilla@doublevpartners.com>
2026-02-03 10:08:58 -0500

    feat: wrap AppTitleHeader in FittedBox for better responsiveness

--- lib/design/atomic/atoms/headers/app_title_header.dart
+++ lib/design/atomic/atoms/headers/app_title_header.dart
@@ -11,29 +11,32 @@ class AppTitleHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     final double titleSize = context.layout.value(xs: 22, md: 25, lg: 28);
 
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: Gutter.separateChildren(
-        children: <Widget>[
-          Text(
-            'STOYCO',
-            style: FontFoundation.title.mediumSaLight.copyWith(
-              fontSize: titleSize,
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: Gutter.separateChildren(
+          children: <Widget>[
+            Text(
+              'STOYCO',
+              style: FontFoundation.title.mediumSaLight.copyWith(
+                fontSize: titleSize,
+              ),
             ),
-          ),
-          StoycoAssetsToken.lib.assets.images.stoycoOnboardingLogo.image(
-            package: 'stoyco_partners_shared',
-            width: 95,
-            height: 152,
-          ),
-          Text(
-            'PARTNERS',
-            style: FontFoundation.title.mediumSaLight.copyWith(
-              fontSize: titleSize,
+            StoycoAssetsToken.lib.assets.images.stoycoOnboardingLogo.image(
+              package: 'stoyco_partners_shared',
+              width: 95,
+              height: 152,
             ),
-          ),
-        ],
-        extent: 18,
+            Text(
+              'PARTNERS',
+              style: FontFoundation.title.mediumSaLight.copyWith(
+                fontSize: titleSize,
+              ),
+            ),
+          ],
+          extent: 18,
+        ),
       ),
     );
   }
